a80fb42266a76c0eef31163e9a9e32fadf694c93
Jennifer washburn <jjw244@users.noreply.github.com>
2017-06-15 17:32:05 -0500

    User Story 3 - Mentor Availability - COMPLETE

--- lib/kele.rb
+++ lib/kele.rb
@@ -13,10 +13,18 @@ class Kele
 
   def get_me
     response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token})
-    @user = JSON.parse(response.body)
+    @user = JSON.parse('[response.body]')
   end
-  
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token})
+    @mentor_availability = JSON.parse(response.body)
+    p @mentor_availability
+  end
+
   private
+
+  # Charlie mentor_id: 529277
 
   def api_url(endpoint)
     "https://www.bloc.io/api/v1/#{endpoint}"
