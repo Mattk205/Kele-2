e4a8302ee9a62bf1afdf270662c59e2932ac4013
Jennifer washburn <jjw244@users.noreply.github.com>
2017-06-16 09:17:15 -0500

    User Story 5 - Messaging - COMPLETE

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,7 +5,7 @@ require './lib/roadmap.rb'
 class Kele
   include HTTParty
   include Roadmap
-  # base_uri = 'https://www.bloc.io/api/v1'
+  base_uri = 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
     options = { body: { email: email, password: password } }
@@ -15,14 +15,37 @@ class Kele
   end
 
   def get_me
-    response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token})
+    response = self.class.get(api_url("users/me"),
+      headers: { "authorization" => @auth_token })
     @user = JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id)
-    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token})
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"),
+      headers: { "authorization" => @auth_token })
     @mentor_availability = JSON.parse(response.body)
     p @mentor_availability
+  end
+
+  def get_messages(arg = nil)
+    if arg == nil
+      response = self.class.get(api_url("message_threads"),
+        headers: { "authorization" => @auth_token })
+    else
+      response = self.class.get(api_url("message_threads?page=#{arg}"),
+        headers: { "authorization" => @auth_token })
+    end
+    @messages = JSON.parse(response.body)
+  end
+
+  def create_message(sender_email, recipient_id, subject, message)
+    response = self.class.post(api_url("messages"),
+      body: {
+        sender: sender_email,
+        recipient_id: recipient_id,
+        subject: subject,
+        "stripped-text": message },
+      headers: { "authorization" => @auth_token })
   end
 
   private
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,13 +1,15 @@
 module Roadmap
 
   def get_roadmap(roadmap_id)
-    response = self.class.get(api_url("roadmaps/#{roadmap_id}"), headers: { "authorization" => @auth_token})
+    response = self.class.get(api_url("roadmaps/#{roadmap_id}"),
+      headers: { "authorization" => @auth_token})
     @roadmaps = JSON.parse(response.body)
     # p @roadmaps
   end
 
   def get_checkpoint(checkpoint_id)
-    response = self.class.get(api_url("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token})
+    response = self.class.get(api_url("checkpoints/#{checkpoint_id}"),
+      headers: { "authorization" => @auth_token})
     @checkpoints = JSON.parse(response.body)
     # p @checkpoints
   end
